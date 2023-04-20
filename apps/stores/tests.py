b0ae827c70bb2590fc331c9cbaed4d047aad4ea2
gghotted <gghotted2@naver.com>
2023-04-21 04:35:17 +0900

    Feat: store delete api 추가

--- apps/stores/tests.py
+++ apps/stores/tests.py
@@ -206,3 +206,51 @@ class StoreUpdateAPITestCase(BaseTestCase):
             res403_schema,
             auth_user=new_user,
         )
+
+
+class StoreDeleteAPITestCase(BaseTestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = cls.create_user()
+        cls.store = Store.objects.create(owner=cls.user, name="store")
+        cls.path = reverse("stores:detail", args=[cls.store.id])
+
+    def test_url(self):
+        self.assertEqual("/stores/%d/" % self.store.id, self.path)
+
+    def test_success(self):
+        """
+        정상 삭제
+        """
+        self.generic_test(
+            self.path,
+            "delete",
+            204,
+            expected_schema=None,
+            auth_user=self.user,
+        )
+        self.assertFalse(Store.objects.filter(id=self.store.id).exists())
+
+    def test_no_auth(self):
+        """
+        인증 없이
+        """
+        self.generic_test(
+            self.path,
+            "delete",
+            401,
+            res401_schema,
+        )
+
+    def test_not_owner(self):
+        """
+        owner가 아닌
+        """
+        new_user = self.create_user(phone="01098765432")
+        self.generic_test(
+            self.path,
+            "delete",
+            403,
+            res403_schema,
+            auth_user=new_user,
+        )
