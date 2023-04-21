ba63c77527fd9dc4b1278a4653d65e46aac8036f
gghotted <gghotted2@naver.com>
2023-04-21 20:19:02 +0900

    Feat: expected_query_count 추가

--- apps/core/tests.py
+++ apps/core/tests.py
@@ -1,9 +1,27 @@
+import os
 from typing import Optional
 
-from django.test import TestCase
+from django.db import connection, reset_queries
+from django.test import TestCase, override_settings
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from schema import Schema
 from users.models import User
+
+
+def assert_query_count(count):
+    
+    def decorator(func):
+
+        @override_settings(DEBUG=True)
+        def wrapper(*args, **kwargs):
+            reset_queries()
+            ret = func(*args, **kwargs)
+            queries = connection.queries
+            assert len(queries) == count, "query_count:%d != %d" % (len(queries), count)
+            return ret
+    
+        return wrapper
+    return decorator
 
 
 class BaseTestCase(TestCase):
@@ -15,6 +33,7 @@ class BaseTestCase(TestCase):
         method: str,
         expected_status_code: int,
         expected_schema: Schema,
+        expected_query_count: Optional[int] = None,
         auth_user: Optional[User] = None,
         **data,
     ) -> dict:
@@ -27,13 +46,20 @@ class BaseTestCase(TestCase):
             예상되는 결과의 상태 코드
         - expected_schema
             예상되는 결과의 스키마
+        - expected_query_count
+            예상되는 쿼리 수, 값이 주어지고 같지 않을 시 assert
         - auth_user
             인증 유저, 주어지면 access token을 header에 추가
+            
 
         return: response json data
         """
 
         request = getattr(self.client, method)
+
+        if expected_query_count:
+            request = assert_query_count(expected_query_count)(request)
+
         res = request(
             path,
             data=data,
